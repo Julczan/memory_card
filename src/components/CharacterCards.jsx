11d3576d6b509c1a820e26051aca43703082008b
Julczan <julek111222h@gmail.com>
2026-02-28 12:38:13 +0100

    shuffle character array on click

--- src/components/CharacterCards.jsx
+++ src/components/CharacterCards.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import useFetchCharacters from "./FetchCharacters";
+import shuffleArray from "./shuffleArray";
 
 function CharacterCards() {
+  const [clicked, setClicked] = useState([]);
   const { imageUrl, error, loading } = useFetchCharacters();
+
+  function handleClick(id) {
+    console.log(`clicked ${id}`);
+    setClicked([...clicked, id]);
+  }
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
 
+  shuffleArray(imageUrl);
+
   return imageUrl.map((obj) => (
-    <div key={obj.id}>
+    <div key={obj.id} onClick={() => handleClick(obj.id)}>
       <img src={obj.image}></img>
     </div>
   ));
